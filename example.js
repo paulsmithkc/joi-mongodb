0d4e3e1b8cd81a51b1cf3bb50e62d181564ed826
Paul Smith <paulsmithkc@gmail.com>
2021-10-05 07:48:50 -0500

    customizing the error message text

--- example.js
+++ example.js
@@ -2,21 +2,24 @@ const Joi = require('joi');
 require('./index')(Joi);
 
 const { ObjectId } = require('mongodb');
-const schema = Joi.objectId().required();
+const schema = Joi.object({
+  id: Joi.objectId().required(),
+});
 const testData = [
   undefined,
   null,
   false,
+  true,
   0,
   '',
   123,
   '123',
   'abc',
   ObjectId(),
-  ObjectId().toString()
+  ObjectId().toString(),
 ];
 
 console.log(testData);
 
-const results = testData.map(x => schema.validate(x));
+const results = testData.map((x) => schema.validate({ id: x }));
 console.log(results);
--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const { ObjectId } = require('mongodb');
 
 module.exports = (Joi) => {
   Joi.objectId = () =>
-    Joi.any().custom((value, helpers) => {
-      try {
-        if (!value) {
-          throw new Error('ObjectId was falsy');
-        } else if (typeof value !== 'string' && typeof value !== 'object') {
-          throw new Error('ObjectId was wrong type');
-        } else {
-          return new ObjectId(value);
+    Joi.any()
+      .custom((value, helpers) => {
+        try {
+          if (!value) {
+            throw new Error('ObjectId was falsy');
+          } else if (typeof value !== 'string' && typeof value !== 'object') {
+            throw new Error('ObjectId was wrong type');
+          } else {
+            return new ObjectId(value);
+          }
+        } catch {
+          return helpers.error('any.objectId');
         }
-      } catch {
-        throw new Error('ObjectId was invalid');
-      }
-    }, 'objectId');
+      }, 'ObjectId')
+      .rule({
+        message: {
+          'any.objectId': '{#label} was not a valid ObjectId',
+        },
+      });
 };
