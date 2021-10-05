8faf5f6b77e9610490c7ead30e3b9690df9cd52f
Paul Smith <paulsmithkc@gmail.com>
2021-10-05 01:05:21 -0500

    sending the same message in all invalid cases

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const { ObjectId } = require('mongodb');
 module.exports = (Joi) => {
   Joi.objectId = () =>
     Joi.any().custom((value, helpers) => {
-      if (value === null) {
-        throw new Error('ObjectId was null');
-      } else if (typeof value !== 'string' && typeof value !== 'object') {
-        throw new Error('ObjectId was wrong type');
-      } else {
-        try {
+      try {
+        if (!value) {
+          throw new Error('ObjectId was falsy');
+        } else if (typeof value !== 'string' && typeof value !== 'object') {
+          throw new Error('ObjectId was wrong type');
+        } else {
           return new ObjectId(value);
-        } catch {
-          throw new Error('ObjectId was invalid');
         }
+      } catch {
+        throw new Error('ObjectId was invalid');
       }
     }, 'objectId');
 };
